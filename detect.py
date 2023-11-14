bbdb098bf716983a932be5aba3d8db8f91a3b4f3
gae <gaedev22@gmail.com>
2023-11-14 16:38:12 +0900

    delete unnecessary files

--- detect.py
+++ detect.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 # model = YOLO("yolov8s.pt")  # 物体検知
-# model = YOLO("yolov8s-seg.pt")  # セグメンテーション
-model = YOLO("yolov8s-pose.pt")  # 姿勢推定
+model = YOLO("yolov8s-seg.pt")  # セグメンテーション
+# model = YOLO("yolov8s-pose.pt")  # 姿勢推定
 
 results = model(0, show=True)
