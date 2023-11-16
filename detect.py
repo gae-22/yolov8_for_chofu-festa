52b7e538fa0828c9a36b4fb7634f0340a7300d58
gae <gaedev22@gmail.com>
2023-11-16 21:22:58 +0900

    change model

--- detect.py
+++ detect.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 
-# model = YOLO("yolov8s.pt")  # 物体検知
-model = YOLO("yolov8s-seg.pt")  # セグメンテーション
-# model = YOLO("yolov8s-pose.pt")  # 姿勢推定
+# model = YOLO("yolov8m.pt")  # 物体検知
+# model = YOLO("yolov8m-seg.pt")  # セグメンテーション
+model = YOLO("yolov8m-pose.pt")  # 姿勢推定
 
 results = model(0, show=True)
